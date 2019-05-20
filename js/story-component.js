f70a870adeea602b9d407a8e99e9a939ec5d8a1f
Chinh <contact@chinhdo.co>
2019-05-20 23:40:02 +1000

    Alternated dialogs. Simple js-generated dialog

--- js/story-component.js
+++ js/story-component.js
@@ -3,6 +3,16 @@
 // });
 
 $(document).ready(function () {
-    console.log("ready!");
-    
+    var $gameViewport = $('.game-viewport');
+    var $dialogContainer = $('<div>').addClass('dialog-container');
+    var $characterPortraitContainer = $('<div>').addClass('character-portrait-container');
+    var $characterPortrait = $('<img>').addClass('character-portrait');
+    var $dialogBubble = $('<div>').addClass('dialog-bubble');
+
+    $('article').append($dialogContainer);
+    $characterPortraitContainer.appendTo($dialogContainer);
+    $characterPortraitContainer.append($characterPortrait);
+    $characterPortraitContainer.append('Name');
+    $dialogBubble.appendTo($dialogContainer);
+    $dialogBubble.append('This is dialog number 2');
 });

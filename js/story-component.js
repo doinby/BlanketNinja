ab12cd48f33d9d71d2c269e24563e0b82503e7d1
Chinh <contact@chinhdo.co>
2019-06-05 23:12:49 +1000

    basic layout setup in progress

--- js/story-component.js
+++ js/story-component.js
@@ -1,29 +1,56 @@
 $(document).ready(function () {
+    
+    // Declare CSS Variables ////////////////////////////
+    
     var $gameViewport = $('.game-viewport');
     var $backgroundImage = $('.background-image');
     var $dialogContainer = $('<li>').addClass('dialog-container');
     var $characterPortraitContainer = $('<div>').addClass('character-portrait-container');
     var $characterPortrait = $('<img>').addClass('character-portrait');
     var $dialogBubble = $('<div>').addClass('dialog-bubble');
-
-    var currentPage;
-
-    $backgroundImage.fadeIn("slow");
-
-    function updateCurrentPage(pageNumber) {
-        currentPage = pageNumber;
-        console.log(currentPage);
-    }
-
-    function appendDialog () { 
-    }
     
-    $gameViewport.bind('scroll', function () {
-        if ($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
-            console.log("reaches the bottom!")
+    // Declare JS Variables /////////////////////////////
+    
+    var currentPage = 0;
+    var currentDialog;
+    
+    // Setup Story Component ////////////////////////////
+    function StorySetup() {
+        switch (progress) {
+            case 0:
+            updateCurrentBackgroundImage(currentPage);
+            break;
+            case 1:
+            
+            break;
+            case 2:
+            
+            break;
+            case 3:            
+            
+            break;
+            case 4:            
+            
+            break;
+            case 5:
+            
+            break;
         }
-    });
+        progress++;
+    }
     
+    function updateCurrentBackgroundImage (i) {
+        i = currentPage;
+        $gameViewport.css({
+            'background-image': "url(../images/background-" + i + ".png)",
+        });
+    }
+    
+    function currentDialog() { 
+        
+    }
+
+    updateCurrentBackgroundImage();
     // appendDialogContainer();
-    updateCurrentPage(0);
+    $backgroundImage.fadeIn("slow");
 });

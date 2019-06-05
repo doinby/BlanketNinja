0021c89e234c435a769de337b121dfae9bc4c692
Chinh <contact@chinhdo.co>
2019-06-05 22:34:13 +1000

    deleted old dialog spawning

--- js/story-component.js
+++ js/story-component.js
@@ -15,52 +15,7 @@ $(document).ready(function () {
         console.log(currentPage);
     }
 
-    function appendDialog () {  
-        for (i = 0; i <= 3; i++) {
-            var $makeDialogContainer = $('<li>')
-            .addClass('dialogContainer');
-            var $makeCharacterPortraitContainer = $('<div>')
-            .addClass('character-portrait-container');
-            var $makeCharacterPortrait = $('<img>')
-            .addClass('character-portrait')
-            .css({
-                width: 256,
-                height: 256,
-                // 'background-color': "salmon"
-            });
-            var $makeDialogBubble = $('<div>')
-            .addClass('dialog-bubble');
-            switch (i) {
-                //assuming there are establish scene
-                case 0:
-                name = "Oliver";
-                imageFile = name;
-                txt = "Woah! this roof is super slippery!";
-                break;
-                case 1:
-                name = "Remy";
-                imageFile = name;
-                txt = "Careful not to wake up the kitten!";
-                break;
-                case 2:
-                name = "Oliver";
-                imageFile = name;
-                txt = "Where are we going, big brother?";
-                break;
-                case 3:
-                name = "Remy";
-                imageFile = name;
-                txt = "We should start with Otter's house at the corner left.";
-                break;
-            }            
-            // $makeDialogContainer.appendTo ($storyComponent);
-            $makeDialogContainer.append ($makeCharacterPortraitContainer);
-            $makeDialogContainer.append ($makeDialogBubble);
-            $makeCharacterPortraitContainer
-            .append($makeCharacterPortrait.attr ('src', "images/" + imageFile + ".png"));
-            $makeCharacterPortraitContainer.append (name);
-            $makeDialogBubble.append (txt);
-        }
+    function appendDialog () { 
     }
     
     $gameViewport.bind('scroll', function () {

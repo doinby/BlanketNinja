f2df669625237257d8e268e93dd6030b04f0b2ef
Chinh <contact@chinhdo.co>
2019-06-06 22:41:18 +1000

    remove unfixed problems

--- js/story-component.js
+++ js/story-component.js
@@ -19,51 +19,7 @@ $(document).ready(function () {
     var currentDialog;
     
     // Setup Story Component ////////////////////////////    
-    function spawnSceneData (currentPage) {
-        var sceneProgress = 0;
-        switch (sceneProgress) {
-            case 0:
-            sceneCount = 5;
-            // spawnSceneImages(currentPage, sceneCount);
-            case 1:
-            sceneCount = 4;
-            case 2:
-            sceneCount = 3;
-        }
-        sceneProgress++;
-    }
     
-    // function spawnSceneImages(currentPage, sceneCount) {
-    //     for (i = 0; i <= sceneCount; i++) {      
-    //         $sceneContainer = $('<div>')
-    //         .addClass('scene-container --flex')
-    //         .appendTo($('.more-container'));
-    
-    //         $sceneImages = $('<img>')
-    //         .addClass('scene-images')
-    //         .attr('src', "../images/scenes/scene" + currentPage + "-" + i + ".png")
-    //         .appendTo($sceneContainer);
-    //     }
-    // }
-    
-    function spawnSceneImages() {
-        var i = 0;
-        
-        $sceneContainer = $('<div>')
-        .addClass('scene-container --flex')
-        .appendTo($('.more-container'));
-        
-        $sceneImages = $('<img>')
-        .addClass('scene-images')
-        .attr('src', "../images/scenes/scene0-" + i + ".png")
-        .appendTo($sceneContainer);
-        
-        i++;
-        console.log(i);
-    }
-    
-    // spawnSceneData(0);
-    // $gameViewport.mousedown (spawnSceneImages());
     $gameViewport.mousedown( function() {
         console.log("clicked");
     });

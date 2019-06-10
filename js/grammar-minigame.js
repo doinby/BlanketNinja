4dafc329a6fb4ffb34a7f1c04522ed54b3b7632a
Chinh <contact@chinhdo.co>
2019-06-11 01:11:27 +1000

    grammar minigame win/lose condition finished

--- js/grammar-minigame.js
+++ js/grammar-minigame.js
@@ -10,20 +10,41 @@ $(document).ready(function () {
     var $notificationBtn = $('<a>').addClass('button --flex --centerElement');
     var $blockedOut = $('.blocked-out');
     var $answer = $('.answer');
-    var value;
+    var $wasDragged;
     
     // Declare JS Variables /////////////////////////////
     
+    var value;
     var title = document.getElementsByTagName("title")[0].innerHTML;
-    var text = 'We choose to go to the moon. We <div></div> to the moon in this decade and do the other things, <div></div> because they are easy, <div></div> because they are hard, because that goal will serve to organize and measure the best of our <div></div> and skills, because that challenge is one that we are willing to accept, one we <div></div> to postpone, and one which we intend to win, and the others, too.'
-    
+    var quote = 'We choose to go to the moon. We <div></div> to the moon in this decade and do the other things, <div></div> because they are easy, <div></div> because they are hard, because that goal will serve to organize and measure the best of our <div></div> and skills, because that challenge is one that we are willing to accept, one we <div></div> to postpone, and one which we intend to win, and the others, too.'
+    var author = "hiii";
     var myAnswer;
+    var isFilled = 0;
     
     // Setup Scene //////////////////////////////////////
     
     // Game Parameters
     var possibilities = ["willing", "choosen", " energize", "choose", "not", "though", "energies", "are unwilling", "but"];
     var correctAnswers = ["choose", "not", "but", "energies", "are unwilling"]
+    
+    switch (title) {
+        case "Blanket Ninja - Math Challenge X":
+        $notificationText.text("Keep a close googly-eye on the number of turns you have left");
+        break;
+        
+        case "Blanket Ninja - Math Challenge Y":
+        $notificationText.text("Multiply an '8' with an 3-digits number to get an 4-digits number.");
+        break;
+        
+        case "Blanket Ninja - Math Challenge Z":
+        $notificationText.text("You are a math genius, you can do it!");
+        break;
+    }
+    
+    // Shuffle Array
+    possibilities.sort(function() {
+        return 0.5 - Math.random(); 
+    });
     
     // Add Hint Button
     $hintBtn = $('<a>')
@@ -34,19 +55,25 @@ $(document).ready(function () {
     })
     .text("Hint");
     
-    $('blockquote').append(text);
+    // Add Grammar CHallenge
+    $questionContainer = $('<div>')
+    .addClass('tile question-contaner has-background-light --flex')
+    .append('<blockquote>' + quote, '<cite>' + author)
+    .appendTo($gameViewport);
+    
+    // $('blockquote').append(text);
     $('blockquote > div').addClass('blocked-out');
     
-    var $draggedElement;
     for (i = 0; i < possibilities.length; i++) {
         $answer = $('<div>')
         .addClass('possibilities --flex --centerElement')
         .appendTo($('.answer-container'))
         .draggable({
-            snap: $('.blocked-out'),
+            helper: "clone",
+            // snap: $('.blocked-out'),
             revert: "invalid",
             drag: function(event, ui) {
-                $draggedElement = $(this);
+                $wasDragged = $(this);
                 value = $(this).text();
             }
         })
@@ -54,17 +81,27 @@ $(document).ready(function () {
     }
     
     $('.blocked-out').droppable({
-        accept: $('.possibilities'),
+        accept: $('*'),
         drop: function (event, ui) {
-            $(this).text(value);   
+            $(this)
+            .addClass('--incorrect')
+            .text(value);   
             myAnswer = $(this).text();
             if (checkAnwer($(this).index())) {
-                $(this).addClass('--correct');
-            } else {
-                $(this).addClass('--incorrect');  
+                $(this)
+                .addClass('--correct')
+                .toggleClass('--incorrect');
+            }  
+            if (isFilled == correctAnswers.length) {
+                gameOver("Win");
             }
-            $draggedElement.remove();
         }
+    });
+    
+    $('.blocked-out').draggable({
+        helper: "clone",
+        // snap: $('.blocked-out'),
+        revert: "invalid"
     });
     
     // Game Controller //////////////////////////////////
@@ -144,11 +181,26 @@ $(document).ready(function () {
         }
     }
     
+    // Compare Dropped Answer with Answer List
     function checkAnwer(x) {
         if (correctAnswers[x] == myAnswer) {
+            isFilled++;
             return true;
         } else {
             return false;
+        }
+    }
+    
+    function gameOver(result) {
+        $('.hint-btn').off('click');
+        $('.possibilities, .blocked-out').draggable("destroy");
+        switch (result) {
+            case "Win":
+            spawnNotifications(result);
+            break;
+            case "Lose":
+            spawnNotifications(result);
+            break;
         }
     }
 });
